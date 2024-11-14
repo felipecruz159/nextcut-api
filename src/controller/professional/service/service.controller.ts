cb238ceb09d0b0b0d00e718d6994c933de976732
RafaGms <faelgomes2003@gmail.com>
2024-11-13 23:56:59 -0300

    ✨feat: routes to show service to customer

--- src/controller/professional/service/service.controller.ts
+++ src/controller/professional/service/service.controller.ts
@@ -42,4 +42,43 @@ export default {
          return res.status(500).json({ message: "Erro interno do servidor." });
       }
    },
+
+   async getServicesClient(req: Request, res: Response): Promise<Response> {
+      const { barberShopId } = req.params;
+
+      if (!barberShopId) {
+         return res.status(400).json({ message: "barberShopId é obrigatório." });
+      }
+
+      try {
+         const barbershop = await db.barbershop.findFirst({
+            where: { id: barberShopId },
+         });
+
+         if (!barbershop) {
+            return res.status(404).json({ message: "Nenhuma barbearia encontrada para este usuário." });
+         }
+
+         const services = await db.service.findMany({
+            where: { barbershopId: barbershop.id },
+            select: {
+               id: true,
+               name: true,
+               description: true,
+               category: true,
+               price: true,
+               time: true,
+            },
+         });
+
+         if (services.length === 0) {
+            return res.status(404).json({ message: "Nenhum serviço encontrado para esta barbearia." });
+         }
+
+         return res.json(services);
+      } catch (error) {
+         console.error("Erro ao buscar serviços:", error);
+         return res.status(500).json({ message: "Erro interno do servidor." });
+      }
+   },
 };
--- src/controller/professional/service/service.routes.ts
+++ src/controller/professional/service/service.routes.ts
@@ -4,5 +4,6 @@ import serviceController from './service.controller';
 const router = Router();
 
 router.get('/services/user/:userId', serviceController.getServices);
+router.get('/services/client/:barberShopId', serviceController.getServicesClient);
 
 export default router;
