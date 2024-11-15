3b2dc6dce9bff6c6849dd99c8330afe151400c69
RafaGms <faelgomes2003@gmail.com>
2024-11-15 09:21:10 -0300

    ✨feat: routes for editing the barbershop image

--- src/controller/professional/information/information.controller.ts
+++ src/controller/professional/information/information.controller.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
+import path from 'path';
+import fs from "fs";
 
 const db = new PrismaClient();
 
@@ -50,4 +52,87 @@ export default {
          return res.status(500).json({ message: "Erro interno do servidor." });
       }
    },
+
+   async updateInformationWithImage(req: Request, res: Response): Promise<Response> {
+      const { barbershopId } = req.params;
+      const imageFile = req.file;
+      const { name, specialService, homeService } = req.body;
+
+      if (!barbershopId) {
+         return res.status(400).json({ message: "O ID da barbearia é obrigatório." });
+      }
+
+      try {
+         const barbershop = await db.barbershop.findUnique({
+            where: { id: barbershopId },
+         });
+
+         if (!barbershop) {
+            return res.status(404).json({ message: "Barbearia não encontrada." });
+         }
+
+         const imageFolderPath = path.join(__dirname, "..", "..", "..", "public", "barberShop");
+
+         if (barbershop.imageUrl && imageFile) {
+            const oldImagePath = path.join(imageFolderPath, barbershop.imageUrl);
+            if (fs.existsSync(oldImagePath)) {
+               fs.unlinkSync(oldImagePath);
+               console.log(`Imagem antiga removida: ${oldImagePath}`);
+            }
+         }
+
+         const updateData: any = {
+            name,
+            specialService: specialService === 'true',
+            homeService: homeService === 'true',
+         };
+
+         if (imageFile) {
+            updateData.imageUrl = imageFile.filename;
+         }
+
+         const updatedBarbershop = await db.barbershop.update({
+            where: { id: barbershopId },
+            data: updateData,
+         });
+
+         return res.status(200).json({
+            message: "Informações atualizadas com sucesso.",
+            data: updatedBarbershop,
+         });
+      } catch (error) {
+         console.error("Erro ao atualizar informações:", error);
+         return res.status(500).json({ message: "Erro interno do servidor." });
+      }
+   },
+
+   async InformationClient(req: Request, res: Response): Promise<Response> {
+      const { barbershopId } = req.params; console.log(barbershopId)
+
+      if (!barbershopId) {
+         return res.status(400).json({ message: "barbershopId é obrigatório." });
+      }
+
+      try {
+         const barbershop = await db.barbershop.findUnique({
+            where: { id: barbershopId },
+            select: {
+               about: true,
+               phone: true,
+               operation: true,
+               address: true
+            },
+         });
+
+         if (!barbershop) {
+            return res.status(404).json({ message: "Barbearia não encontrada." });
+         }
+
+         return res.status(200).json({ data: barbershop });
+      } catch (error) {
+         console.error("Erro ao buscar informações:", error);
+         return res.status(500).json({ message: "Erro interno do servidor." });
+      }
+   }
+
 };
--- src/controller/professional/information/information.routes.ts
+++ src/controller/professional/information/information.routes.ts
@@ -1,9 +1,16 @@
 // routes/informationRoutes.ts
 import { Router } from 'express';
 import informationController from './information.controller';
+import multer from 'multer';
+import multerConfig from '../../../config/multer';
 
 const router = Router();
+const upload = multer(multerConfig);
 
 router.put('/information/edit/:barbershopId', informationController.updateInformation);
+
+router.put('/informationWithImage/edit/:barbershopId', upload.single('barberShopBackground'), informationController.updateInformationWithImage);
+
+router.get('/information/client/:barbershopId', informationController.InformationClient);
 
 export default router;
